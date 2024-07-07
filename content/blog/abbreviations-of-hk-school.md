5a647679da38cf440c23022103f1c25ed4ad713d
Wynston Hou <houyewei@gmail.com>
2024-07-07 23:27:44 +0800

    add blog

---
+++ content/blog/abbreviations-of-hk-school.md
@@ -0,0 +1,35 @@
+---
+title: "My First Post"
+date: "2021-07-23"
+slug: "abbreviations-of-hk-school"
+---
+
+对于没有经历过香港教育的我看着关于学校各种英文字母从一头雾水到反复记不住（就像当年上学被英文单词一样)
+
+P班/PG班：playgroup，主要靠钞能力，部分热门playgroup因为有升学幼稚园的优势，需要排队比较久。
+
+PN/N班：pre-nursery/nursery班，即2岁入学的幼稚园班
+
+K：K1-K3即幼稚园，通常3-6岁入读，政府三年幼稚园补贴即是这三年
+
+IBDP/IB课程：通常大家说的IB全名International Baccalaureate Diploma Programme 即国际文凭大学预科课程。通常中学最后两年才读这个课程。
+
+IBMYP：IB Middle Years Programme。是为 11-16 岁的中学生开设的一项为期五年的严格课程。根据与学校的合作情况，学生可以参加缩略版的 MYP 课程，如两年、三年或四年制的课程。
+
+A-Level/IAL：英国制高中课程（General Certificate of Education Advanced Level），简称GCE A-Level课程，是英国本土的全民高中课程体系。在英国，要入大学深造，一般在第12及13班开始学习这种课程，需要完成两年制的A-Level课程才可。IAL就是International A-Level是供在英国境外学习的学生参加的高中毕业考试。
+
+IGCSE：全名International General Certificate of Secondary Education即国际中等教育普通证书，通常1-2年，于中四-中五学习.学生在完成IGCSE后，其学业上的程度相当于香港的中五。所以，IGCSE的成绩不能直接用来申请大学，学生通过IGCSE考试后需要继续报读GCE A-Level、IAL（International Advanced Levels）或IBDP课程，之后再使用A-Level、IAL、IBDP成绩申请入读国外的大学。而有意留港升学的学生也可经非联招（Non-JUPAS）报读本地大学学位。
+
+DSE：Hong Kong Diploma of Secondary Education (香港中学文凭考试)，就是香港本地所说的文凭试或者内地概念中的香港高考。
+
+彩蛋: A-Level and IB
+
+由以上可以看出，香港中学生如果本地路线就参加DSE。海外升学路线就是考A-Level或者IB。而IB又在其中占了多数。
+
+IB课程是一套从幼儿园到大学预科的课程，旨在培养3至19岁学生的智力、情感、个人发展和社会技能，使其具备学习、工作和生存所需的能力。IB课程分为三个阶段：
+    小学阶段：PYP（Primary Years Programme）课程，专为3至12岁的幼儿园和小学生设计，涵盖广泛的学习项目。
+    中学阶段：MYP课程，适用于6至10年级的中学生，旨在培养综合素质。
+    高中阶段：IBDP课程，为16至19岁的青少年提供全球最具创新性的教育课程。它包括六大科目和三大核心课程。
+有别于国际学校会提供全套IB课程，本地学校通常在中学的最后1-2年提供IBDP，已准备IB考试。A Level也类似，本地学校通常在中学的最后才提供，以应对考试。
+
+此外，一些学校比如哈罗学提供A-Level课程，通常认为是针对升学英国的学生，英基（ESF）提供IB课程，通常也认为是针对升学英国的学生。但是以我个人的观察，学校和课程似乎都不是留学国家的决定因素，比如熟人朋友中有ESF学生去了美国加拿大，哈罗学生去了美国。
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,14 @@
 module.exports = {
   siteMetadata: {
     title: `Discover HK School`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://blog.discoverhkschool.com`,
+    author: {
+      name: `DiscoverHKSchool`,
+      summary: `who lives and works in Hong Kon building useful things.`,
+    },
+    social: {
+      twitter: "houyewei"
+    }
   },
   plugins: ["gatsby-plugin-styled-components", { 
     resolve: "gatsby-plugin-google-gtag",
@@ -16,19 +23,112 @@ module.exports = {
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
+  }, 
+  // "gatsby-plugin-mdx", 
+  "gatsby-plugin-sharp", "gatsby-transformer-sharp", 
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
       "path": "./src/pages/"
     },
     __key: "pages"
+  },     {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/content/blog`,
+      name: `blog`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  }, {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 630,
+          },
+        },
+        {
+          resolve: `gatsby-remark-responsive-iframe`,
+          options: {
+            wrapperStyle: `margin-bottom: 1.0725rem`,
+          },
+        },
+        `gatsby-remark-prismjs`,
+      ],
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-feed`,
+    options: {
+      query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+      feeds: [
+        {
+          serialize: ({ query: { site, allMarkdownRemark } }) => {
+            return allMarkdownRemark.nodes.map(node => {
+              return Object.assign({}, node.frontmatter, {
+                description: node.excerpt,
+                date: node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + node.fields.slug,
+                guid: site.siteMetadata.siteUrl + node.fields.slug,
+                custom_elements: [{ "content:encoded": node.html }],
+              })
+            })
+          },
+          query: `{
+            allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+              nodes {
+                excerpt
+                html
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                  date
+                }
+              }
+            }
+          }`,
+          output: "/rss.xml",
+          title: "Gatsby Starter Blog RSS Feed",
+        },
+      ],
+    },
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Gatsby Starter Blog`,
+      short_name: `Gatsby`,
+      start_url: `/`,
+      background_color: `#ffffff`,
+      // This will impact how browsers show your PWA/website
+      // https://css-tricks.com/meta-theme-color-and-trickery/
+      // theme_color: `#663399`,
+      display: `minimal-ui`,
+      icon: `src/images/icon.png`, // This path is relative to the root of the site.
+    },
   }]
 };
